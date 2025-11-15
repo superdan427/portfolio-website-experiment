82aac5ae0a3f76b1ce98cd95d63d9bb1bb8e7bf1
superdan427 <xudaniel3@gmail.com>
2025-11-15 03:37:07 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// Click and drag the mouse to view the scene from different angles.
+
+function setup() {
+  createCanvas(100, 100, WEBGL);
+
+  describe('A multicolor box on a gray background. The camera angle changes when the user interacts using a mouse, trackpad, or touchscreen.');
+}
+
+function draw() {
+  background(200);
+
+  // Enable orbiting with the mouse.
+  orbitControl();
+
+  // Style the box.
+  normalMaterial();
+
+  // Draw the box.
+  box(30, 50);
+}
